aecfd5beaa7ffe5fde629ff7a870b528e234668b
ishquark13 <ish.boson13@gmail.com>
2016-09-25 23:29:50 -0400

    added the typed.js plugin

--- jquery.js
+++ jquery.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
 	 $('.btn btn-primary').click(function() {
 	 	$('.btn btn-primary').fadeOut('slow');
 	 });
+
+
+        $(".typed").typed({
+            strings: ["First sentence.", "Second sentence."],
+            typeSpeed: 0
+        });
+    });
 });
