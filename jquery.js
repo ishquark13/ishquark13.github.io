ff7f59993a282ab56c0ccd928ae222932ff07103
ishquark13 <ish.boson13@gmail.com>
2016-09-25 23:32:10 -0400

    2

--- jquery.js
+++ jquery.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	 	$('.btn btn-primary').fadeOut('slow');
 	 });
 
-
+	 $(function(){
         $(".typed").typed({
             strings: ["First sentence.", "Second sentence."],
             typeSpeed: 0
